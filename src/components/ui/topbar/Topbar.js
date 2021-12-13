6b0fac1fb041db9a4cd4198501adfacc5ddaa1f6
Kawish Hussain <kawish791@gmail.com>
2021-12-13 16:10:41 +0500

    Add new logo, topbar, and code clean up

---
+++ src/components/ui/topbar/Topbar.js
@@ -0,0 +1,22 @@
+import logo from '../../../logo/logo.svg';
+import "./topbar.css";
+
+export const Topbar = (props) => {
+return (
+  <div className="topbar p-3 flex flex-row justify-between">
+    <div className="logo">
+      <img src={logo} width={200} className={'rounded-xl'}></img>
+    </div>
+
+    <div className="order-last">
+      <div className="flex flex-row items-center">
+        <div className="px-3 cursor-pointer hover:text-rose-800">Home</div>
+        <div className="px-3 cursor-pointer hover:text-rose-800">Profile</div>
+        <div className="px-3 cursor-pointer hover:text-rose-800">Pricing</div>
+        <div className="px-3 cursor-pointer hover:text-rose-800">Contact us</div>
+        <div className="px-3 cursor-pointer hover:text-rose-800 bg-rose-600 text-white p-2 rounded-lg">Changelog</div>
+      </div>
+    </div>
+  </div>
+)
+}
